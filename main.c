94ab77b5375045238a25fb4ac187fbf8d510ef61
leo <papi_leo@papileo.>
2025-03-09 17:00:26 +0000

    Add: archivo main para control del juego

--- main.c
+++ main.c
@@ -1,2 +1,40 @@
 #include "feria.h"
+#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+const char FIN = 'X';
+
+int main() {
+  srand((unsigned)time(NULL));
+  juego_t juego;
+  inicializar_juego(&juego);
+  imprimir_terreno(juego);
+
+  char accion;
+  printf("Movimiento para Perry: ");
+  scanf("%c", &accion);
+  realizar_jugada(&juego, accion);
+  system("clear");
+  imprimir_terreno(juego);
+
+  bool es_valida = true;
+  while (es_valida) {
+
+    imprimir_terreno(juego);
+    printf("Movimiento para Perry: ");
+    printf("\n");
+    scanf("%c", &accion);
+    realizar_jugada(&juego, accion);
+
+    if (accion == 'X') {
+      es_valida = false;
+      printf("Juego terminado");
+    }
+
+    system("clear");
+  }
+
+  return 0;
+}
